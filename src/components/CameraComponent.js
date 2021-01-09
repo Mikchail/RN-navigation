7db69f7ed828daa310b78bd762c3b2b98e4a74b8
Mikchail <refs1@yandex.ru>
2021-01-10 00:33:13 +0400

    add image-picker

--- src/components/CameraComponent.js
+++ src/components/CameraComponent.js
@@ -1,4 +1,5 @@
 import React, {PureComponent} from 'react';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import {
   View,
   Text,
@@ -7,6 +8,8 @@ import {
   SafeAreaView,
   StyleSheet,
   Button,
+  Image,
+  ImageBackgroundBase,
 } from 'react-native';
 import {RNCamera} from 'react-native-camera';
 
@@ -15,59 +18,59 @@ class Camera extends PureComponent {
     super(props);
     this.state = {
       takingPic: false,
+      avatarSource: false,
     };
   }
 
-  takePicture = async () => {
-    if (this.camera && !this.state.takingPic) {
-      const options = { quality: 0.5, base64: true };
+  // More info on all the options is below in the API Reference... just some common use cases shown here
+  takePhoto = () => {
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{name: 'fb', title: 'Choose Photo from Facebook'}],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
 
-      this.setState({takingPic: true});
+    /**
+     * The first arg is the options object for customization (it can also be null or omitted for default options),
+     * The second arg is the callback which sends object: response (more info in the API Reference)
+     */
+    launchCamera(options, (response) => {
+      console.log('Response = ', response);
 
-      try {
-        const data = await this.camera.takePictureAsync(options);
-        console.log(data);
-        Alert.alert('Success', JSON.stringify(data));
-      } catch (err) {
-        Alert.alert('Error', 'Failed to take picture: ' + (err.message || err));
-        this.setState({takingPic: false});
-        return;
-      } finally {
-        this.setState({takingPic: false});
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        const source = {uri: response.uri};
+
+        // You can also display the image using data:
+        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+        this.setState({
+          avatarSource: source,
+        });
       }
-    }
+    });
   };
   render() {
     return (
       <View>
-        <RNCamera
-          ref={(ref) => {
-            this.camera = ref;
-          }}
-          captureAudio={false}
-          style={{flex: 1}}
-          type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
-          androidCameraPermissionOptions={{
-            title: 'Permission to use camera',
-            message: 'We need your permission to use your camera',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          androidRecordAudioPermissionOptions={{
-            title: 'Permission to use audio recording',
-            message: 'We need your permission to use your audio',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
-          }}
-        />
-        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
-          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-            <Text style={{fontSize: 14}}> SNAP </Text>
-          </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            this.takePhoto();
+          }}>
+          <Text>TouchableOpacity</Text>
+        </TouchableOpacity>
+        <View>
+          {Boolean(this.state.avatarSource) && (
+            <Image  style={{width: 200,height: 200}} source={this.state.avatarSource} />
+          )}
         </View>
       </View>
     );
@@ -78,7 +81,6 @@ export const AppCamera = () => {
   return (
     <SafeAreaView>
       <Camera />
-     
     </SafeAreaView>
   );
 };
